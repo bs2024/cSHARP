c22a053d71abc386361783388ee5d1184b38f577
bs2024 <smitsb2020@gmail.com>
2025-02-04 11:14:23 -0600

    commitTwo

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,8 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//creating a variable
+/*creating variables
 var testingVar = "variableValue";
 
 Console.WriteLine(testingVar);
 Console.WriteLine("testing the syntax");
 Console.WriteLine("Hello, World!");
+*/
+
+//datatypes
+int a = 100;
+string b = "abc";
+byte c = 255; //0-255
+char d = 'a';
+double newDouble = 3.141592653539;
+float newFloat = 0.113987f;
+decimal newDecimal = .2323232323m;
+
+int e = a + a;
+Console.WriteLine(e);
+
+int modulous = 8 % 3;
+Console.WriteLine(modulous);
+
+double testingDivide = 9 / 2.0; //need to divide by a float to get correct decimal
+Console.WriteLine(testingDivide);
+
+a += 2; // += -= *=
+Console.WriteLine(a);
+
+newDouble++;
+Console.WriteLine(newDouble);
