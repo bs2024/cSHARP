584ac93670033f75700e84f5445ea6bd7dbffec0
bs2024 <smitsb2020@gmail.com>
2025-02-04 10:49:28 -0600

    commitOne

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//creating a variable
+var testingVar = "variableValue";
+
+Console.WriteLine(testingVar);
+Console.WriteLine("testing the syntax");
+Console.WriteLine("Hello, World!");
